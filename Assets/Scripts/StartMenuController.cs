5387c0eb00b8a231339b4145311e0b79a3883839
👉ChanCheukChi👈 <149117687+TracyC123@users.noreply.github.com>
2026-02-28 18:20:53 -0500

    implement start and exit buttons functions of Main Menu

--- Assets/Scripts/StartMenuController.cs
+++ Assets/Scripts/StartMenuController.cs
@@ -1,12 +1,33 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class StartMenuController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public void onStartClick()
+    public Button startButton;
+    public Button exitButton;
+
+    void Start()
+    {
+        // Direct assignment in code - bypasses dropdown completely
+        if (startButton != null)
+            startButton.onClick.AddListener(StartGame);
+            
+        if (exitButton != null)
+            exitButton.onClick.AddListener(ExitGame);
+    }
+
+    public void StartGame()
     {
         SceneManager.LoadScene("TestScene");
     }
-        
+
+    public void ExitGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
 }
