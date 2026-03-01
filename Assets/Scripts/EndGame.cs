e9fa2cda6609ef4e8e26a1a4058968f4cbf48984
mirandah31 <mirandahong31@gmail.com>
2026-03-01 15:15:50 -0500

    thanks for playing

--- Assets/Scripts/EndGame.cs
+++ Assets/Scripts/EndGame.cs
@@ -7,7 +7,7 @@ public class EndGame : MonoBehaviour
     public GameObject winUI;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == 'Player')
+        if(collision.gameObject.tag == "Player")
         {
             Time.timeScale = 0;
             winUI.SetActive(true);
